2812134ed17107ebaf2bf92988e7337c4f555b9d
Hadi Deriszadeh <hadi.deris.h82@gmail.com>
2022-03-29 16:13:45 +0430

    new update form.

--- script.js
+++ script.js
@@ -1,106 +1,112 @@
-var person = {};
-
-var Confirm = document.getElementById("confirm");
-var Email = document.getElementById("email");
-var Female = document.getElementById("Female");
-var Male = document.getElementById("Male");
-var Password = document.getElementById("Password");
-var Phone_number = document.getElementById("Phone");
-var Prefer = document.getElementById("Prefer");
+var confirm1 = document.getElementById("confirm");
+var email = document.getElementById("email");
+var female = document.getElementById("Female");
+var male = document.getElementById("Male");
+var password = document.getElementById("Password");
+var phoneNumber = document.getElementById("Phone");
+var prefer = document.getElementById("Prefer");
 var fullName1 = document.getElementById("Name");
 var gender = "";
 var register = document.getElementById("register");
 var userName1 = document.getElementById("username");
 
-Female.addEventListener("click", check_radius1);
-Male.addEventListener("click", check_radius2);
-Prefer.addEventListener("click", check_radius3);
+female.addEventListener("click", checkRadius1);
+male.addEventListener("click", checkRadius2);
+prefer.addEventListener("click", checkRadius3);
 register.addEventListener("click", validation);
 
-function check_radius1() {
+function checkRadius1() {
   gender = "Female";
+  document.getElementById("errorgender").innerHTML =
+    "<span><svg class='svg2'><use xlink:href='#ok'></use></svg></span>";
 }
-function check_radius2() {
+
+function checkRadius2() {
   gender = "Male";
+  document.getElementById("errorgender").innerHTML =
+    "<span><svg class='svg2'><use xlink:href='#ok'></use></svg></span>";
 }
-function check_radius3() {
+
+function checkRadius3() {
   gender = "Prefer";
+  document.getElementById("errorgender").innerHTML =
+    "<span><svg class='svg2'><use xlink:href='#ok'></use></svg></span>";
 }
 
 function validation() {
-  person.Confirm = Confirm.value;
-  person.Email = Email.value;
-  person.Female = Female.value;
-  person.Male = Male.value;
-  person.Password = Password.value;
-  person.Phone_number = Phone_number.value;
-  person.Prefer = Prefer.value;
-  person.gender = gender.value;
-  person.register = register.value;
-  person.userName1 = userName1.value;
-  person.fullName1 = fullName1.value;
+  var person = {
+    confirm: confirm1.value,
+    email: email.value,
+    password: password.value,
+    phoneNumber: phoneNumber.value,
+    gender: gender,
+    userName1: userName1.value,
+    fullName1: fullName1.value,
+  };
 
   if (fullName1.value.length < 6) {
-    alert("Error , fullname must be at least 6 characters!");
+    fullName1.value = "";
+    document.getElementById("error1").innerHTML =
+      "<span><svg class='svg1'><use xlink:href='#Capa_1'></use></svg></span>" +
+      "incorrect value";
     return false;
   }
+  document.getElementById("error1").innerHTML =
+    "<span><svg class='svg2'><use xlink:href='#ok'></use></svg></span>";
   if (userName1.value.length < 4) {
-    console.log(fullName1.value);
-    alert("Error , username must be at least 4 characters!");
+    document.getElementById("error2").innerHTML =
+      "<span><svg class='svg1'><use xlink:href='#Capa_1'></use></svg></span>" +
+      "incorrect value";
+    userName1.value = "";
     return false;
   }
-
-  var test = Email.value;
-  if (Email.value.length == 0 || ![...test].includes("@")) {
-    alert("Error , Please enter the email!");
+  var validRegex =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  document.getElementById("error2").innerHTML =
+    "<span><svg class='svg2'><use xlink:href='#ok'></use></svg></span>";
+  if (!email.value.match(validRegex)) {
+    document.getElementById("error3").innerHTML =
+      "<span><svg class='svg1'><use xlink:href='#Capa_1'></use></svg></span>" +
+      "incorrect value";
+    email.value = "";
     return false;
   }
-
-  var list = [...test];
-  var x = 0;
-  var second = [];
-  var first = [];
-  for (let i = 0; i < list.length; i++) {
-    if (list[i] == "@") break;
-    first[i] = list[i];
-  }
-  for (let i = list.indexOf("@") + 1; i < list.length; i++) {
-    second[x] = list[i];
-    x++;
-  }
-
-  if (first.length < 3) {
-    alert("Error , Incorrect email !");
+  document.getElementById("error3").innerHTML =
+    "<span><svg class='svg2'><use xlink:href='#ok'></use></svg></span>";
+  if (password.value.length < 8) {
+    document.getElementById("error4").innerHTML =
+      "<span><svg class='svg1'><use xlink:href='#Capa_1'></use></svg></span>" +
+      "incorrect value";
+    password.value = "";
     return false;
   }
-
-  array1 = ["gmail.com", "yahoo.com"];
-
-  second = second.join("");
-  second = second.toLocaleLowerCase();
-  if (!array1.includes(second)) {
-    alert("Email extension is incorrect");
+  document.getElementById("error4").innerHTML =
+    "<span><svg class='svg2'><use xlink:href='#ok'></use></svg></span>";
+  if (password.value !== confirm1.value) {
+    document.getElementById("error5").innerHTML =
+      "<span><svg class='svg1'><use xlink:href='#Capa_1'></use></svg></span>" +
+      "incorrect value";
+    confirm1.value = "";
     return false;
   }
-
-  if (Password.value.length < 8) {
-    alert("Error , password must be at least 8 characters!");
-    return false;
-  }
-
-  if (Password.value != Confirm.value) {
-    alert("Error , Password does not match!");
-    return false;
-    // validation();
-  }
-
+  document.getElementById("error5").innerHTML =
+    "<span><svg class='svg2'><use xlink:href='#ok'></use></svg></span>";
   if (gender == "") {
-    alert("Choose your gender !");
-    Confirm.value = "";
+    document.getElementById("errorgender").innerHTML =
+      "<span><svg class='svg1'><use xlink:href='#Capa_1'></use></svg></span>" +
+      "incorrect value";
     return false;
   }
+  
   alert("Success registration");
   const json1 = JSON.stringify(person);
-  console.log(json1);
-  alert(JSON.stringify(person));
+  alert(json1);
 }
+
+// const fs = require("fs");
+// fs.writeFile("json.json", json1, (err) => {
+//   if (err) {
+//     throw err;
+//   }
+//   console.log("JSON data is saved.");
+// });
