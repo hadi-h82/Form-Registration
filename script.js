33701093a7c87d31418f6219774c9703d8dd54bb
Hadi Deriszadeh <hadi.deris.h82@gmail.com>
2022-03-31 17:05:56 +0430

    new update .

--- script.js
+++ script.js
@@ -14,24 +14,33 @@ female.addEventListener("click", checkRadius1);
 male.addEventListener("click", checkRadius2);
 prefer.addEventListener("click", checkRadius3);
 register.addEventListener("click", changeValue);
-fullName1.addEventListener("click", validation);
-userName1.addEventListener("click", validation);
-email.addEventListener("click", validation);
-password.addEventListener("click", validation);
-confirm1.addEventListener("click", validation);
+fullName1.addEventListener("click", beforValidation);
+userName1.addEventListener("click", beforValidation);
+email.addEventListener("click", beforValidation);
+password.addEventListener("click", beforValidation);
+confirm1.addEventListener("click", beforValidation);
 
 
-function changeBG(el) {
-  var delayInMilliseconds = 500;
-  var elem = document.getElementById(el);
-  setTimeout(function () {
-    // elem.style.color = color1;
-    elem.style.opacity = 1;
-    elem.style.transition = " opacity 1s ";
-  }, delayInMilliseconds);
-  elem.style.opacity = 0;
+function beforValidation() {
+  register.value = "";
+  validation();
 }
 
+function changeBG(el, el2 = "") {
+  var delayInMilliseconds = 500;
+  var elem = document.getElementById(el);
+  if (el2 != "") var elem2 = document.getElementById(el2);
+  setTimeout(function () {
+    
+    elem.style.opacity = 1;
+    if (el2 != "") elem2.style.borderColor = "#dd0b0b";
+    elem.style.transition = " all 1s";
+    if (el2 != "") elem2.style.transition = " all 1s ";
+  }, delayInMilliseconds);
+  elem.style.opacity = 0;
+  if(el2 != "") 
+    elem2.style.borderColor = "#ccc";
+}
 
 function changeValue() {
   register.value = "run";
@@ -42,36 +51,35 @@ function checkRadius1() {
   gender = "Female";
   document.getElementById("errorgender").innerHTML = "-";
   empty();
-  validation();
 }
 
 function checkRadius2() {
   gender = "Male";
   document.getElementById("errorgender").innerHTML = "-";
   empty();
-  validation();
 }
 
 function checkRadius3() {
   gender = "Prefer";
   empty();
-  validation();
 }
 
 function empty() {
   document.getElementById("errorgender").innerHTML = "-";
 }
 
-function showError(value) {
-  document.getElementById(value).innerHTML =
+function showError(value1, value2) {
+  document.getElementById(value1).innerHTML =
     "<span><svg class='svg1'><use xlink:href='#Capa_1'></use></svg></span>" +
     "incorrect value";
-    changeBG(value);
+  changeBG(value1, value2);
 }
 
-function showVlid(value) {
-  document.getElementById(value).innerHTML =
+function showVlid(value1, value2) {
+  document.getElementById(value1).innerHTML =
     "<span><svg class='svg2'><use xlink:href='#ok'></use></svg></span>";
+  var elem2 = document.getElementById(value2);
+  elem2.style.borderColor = "#ccc";
 }
 
 function validation() {
@@ -87,40 +95,41 @@ function validation() {
 
   if (fullName1.value.length < 6) {
     fullName1.value = "";
-    showError("error1");
+    showError("error1", "Name");
     return false;
   }
-  showVlid("error1");
+  showVlid("error1", "Name");
   if (userName1.value.length < 4) {
-    showError("error2");
+    showError("error2", "username");
     userName1.value = "";
     return false;
   }
   var validRegex =
     /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-  showVlid("error2");
+  showVlid("error2", "username");
   if (!email.value.match(validRegex)) {
-    showError("error3");
+    showError("error3", "email");
     email.value = "";
     return false;
   }
-  showVlid("error3");
+  showVlid("error3", "email");
   if (password.value.length < 8) {
-    showError("error4");
+    showError("error4", "Password");
     password.value = "";
     return false;
   }
-  showVlid("error4");
+  showVlid("error4", "Password");
   if (password.value !== confirm1.value) {
-    showError("error5");
+    showError("error5", "confirm");
     confirm1.value = "";
     return false;
   }
-  showVlid("error5");
+  showVlid("error5", "confirm");
   if (gender == "") {
     showError("errorgender");
     return false;
   }
+
   if (register.value == "run") {
     alert("Success registration");
     const json1 = JSON.stringify(person);
